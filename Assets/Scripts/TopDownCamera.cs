0edc27a3c9ae4bd4764bc16b4278363869eca86c
Franciszek Zych <franc3002@gmail.com>
2025-04-06 19:16:45 +0200

    Make camera top down

---
+++ Assets/Scripts/TopDownCamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    [SerializeField] private float cameraDistance;
+
+    private void Awake()
+    {
+        SetTopDown();
+    }
+
+    private void SetTopDown()
+    {
+        if (!target) return;
+        transform.position = new Vector3(target.position.x, cameraDistance, target.position.z);
+        transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+    }
+}
